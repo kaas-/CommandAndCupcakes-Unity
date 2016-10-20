eaf4f1426080337d1eb6c2a94ad16d7178618fc9
Rasmus Christiansen <rall94.thekaas@gmail.com>
2016-10-20 11:24:04 +0200

    turn stuff

---
+++ CommandAndCupcakes/Assets/Scripts/Turns.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Turns : MonoBehaviour {
+
+    public List<GameObject> gameObjects = new List<GameObject>();
+
+    private bool turnIsExecuting;
+
+	// Use this for initialization
+	void Start () {
+
+        gameObjects = GameObject.FindGameObjectsWithTag("Player").ToList<GameObject>(); //Add all players to a list 
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(turnIsExecuting)
+        {
+            //continue
+        }
+        else
+        {
+            //stay idle
+        }
+	    
+	}
+
+
+
+
+}
