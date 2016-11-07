2a6df5a3a793e7b6700e862ea3a209e175413d89
Rasmus Christiansen <rall94.thekaas@gmail.com>
2016-11-07 14:25:03 +0100

    controller messages

---
+++ CommandAndCupcakes/Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class ControllerManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        AirConsole.instance.onMessage += OnMessage;
+        AirConsole.instance.onConnect += OnConnect;
+        AirConsole.instance.onDisconnect += OnDisconnect;
+    }
+
+    void OnMessage(int device_id, JToken data)
+    {
+        Debug.Log(data);
+
+    }
+	
+    void OnConnect(int device_id)
+    {
+
+    }
+
+    void OnDisconnect(int device_id)
+    {
+
+    }
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
