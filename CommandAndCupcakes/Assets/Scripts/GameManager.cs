b0c56e89b3d85f4b7e56aa1857f80ea09fb7ce50
Rasmus Christiansen <rall94.thekaas@gmail.com>
2016-11-11 09:49:37 +0100

    methods for starting game, pausing and restarting in case of controller disconnect and reconnect, some more turn logic

--- CommandAndCupcakes/Assets/Scripts/GameManager.cs
+++ CommandAndCupcakes/Assets/Scripts/GameManager.cs
@@ -9,11 +9,13 @@ public class GameManager : MonoBehaviour {
 
     [Range(2, 4)][SerializeField] int playerCount = 4;
 
-    //static GameObject[] playerObjects = new GameObject[4];
+    static GameObject[] playerObjects = new GameObject[4];
 
-    private AirConsole airconsole = AirConsole.instance;
     private int currentPlayer;
-    public bool moving;
+    private bool isWaiting = false;
+    private bool isPaused = false;
+    private bool isStarted = false;
+    private bool isMoving = false;
     private enum playerActions
     {
         moveLeft,
@@ -27,31 +29,25 @@ public class GameManager : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 
-        airconsole.onMessage += OnMessage;
-        airconsole.onConnect += OnConnect;
-        airconsole.onDisconnect += OnDisconnect;
-        airconsole.SetActivePlayers(playerCount);
+        AirConsole.instance.onMessage += OnMessage;
+        AirConsole.instance.onConnect += OnConnect;
+        AirConsole.instance.onDisconnect += OnDisconnect;
 
-        //playerObjects = GameObject.FindGameObjectsWithTag("Player"); //Add all players to the array
+        playerObjects = GameObject.FindGameObjectsWithTag("Player"); //Add all players to the array
         currentPlayer = 0;
 
     }
 
-    private void action(string action)
+    void StartGame()
     {
-        
+        AirConsole.instance.SetActivePlayers(playerCount);
+    }
 
-        /*switch(action)
-        {
-            case playerActions.attack:
-                    
-           
-        }
-           */ 
-
-
+    private void Action(int player, string[] actions)
+    {
+        playerObjects[currentPlayer].SendMessage("Action", actions);
     }
 
     private void nextTurn()
@@ -63,30 +59,78 @@ public class GameManager : MonoBehaviour {
             currentPlayer++;
 
         //send message to controller of next player
-        airconsole.Message(airconsole.ConvertPlayerNumberToDeviceId(currentPlayer), "turn");
+        AirConsole.instance.Message(AirConsole.instance.ConvertPlayerNumberToDeviceId(currentPlayer), "turn");
     }
 
     // Update is called once per frame
     void Update () {
-
+        
+        if (!isMoving && !isWaiting && !isPaused)
+        {
+            nextTurn();
+            isWaiting = true;
+        }
 	}
 
     void OnMessage(int device_id, JToken data)
     {
         Debug.Log(data);
 
+        if (!isStarted)
+        {
+            if (data["action_1"].Equals("start_game"))
+            {
+                StartGame();
+            }
+        }
+        else
+        {
+            if (AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id) == AirConsole.instance.ConvertPlayerNumberToDeviceId(currentPlayer))
+            {
 
+                //execute turn
+                string[] actions = new string[2];
+                actions[0] = (string)data["action_1"];
+                actions[1] = (string)data["action_2"];
 
+                Action(currentPlayer, actions);
+
+                isWaiting = false;
+            }
+            else
+            {
+                //wtf
+            }
+        }
+    }
+
+    void OnPlayerFinishedMoving()
+    {
+        isMoving = false;
     }
 
     void OnConnect(int device_id)
     {
-
+        if (AirConsole.instance.GetActivePlayerDeviceIds.Count == 0)
+        {
+            if (AirConsole.instance.GetControllerDeviceIds().Count > 4)
+            {
+                //too many players
+            }
+            else
+            {
+                playerCount = AirConsole.instance.GetControllerDeviceIds().Count;
+            }
+        }
+        else if (AirConsole.instance.GetControllerDeviceIds().Count == playerCount)
+        {
+            isPaused = false;
+        }
     }
 
     void OnDisconnect(int device_id)
     {
-
+        isPaused = true;
     }
 
     void onActivePlayersChange(int device_id)
