a3b1918acac851b6780982d87dd6d38ee9a0c5ff
pip997 <pipp9997@gmail.com>
2016-12-03 15:19:53 +0100

    Progress on final build for Main screen splash screens.

--- CommandAndCupcakes/Assets/Scripts/GameManager.cs
+++ CommandAndCupcakes/Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using NDream.AirConsole;
 using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -17,8 +18,12 @@ public class GameManager : MonoBehaviour {
     static GameObject[] playerObjects = new GameObject[4];
 
     private int currentPlayer;
-    public Camera[] cameras = new Camera[6]; //an array with all the cameras in the game, [red, blue, green, yellow, game, battleStart]
-    private Camera[] cameraTemp = new Camera[6];
+    public Image imageToChange;
+    public Sprite[] players = new Sprite[4];
+    public Sprite[] combat = new Sprite[6];
+    public Sprite[] endGame = new Sprite[4];
+    public Camera mainCamera;
+    public Camera splashCamera;
     private float t;
     private int lastPlayer;
     private int count = 0;
@@ -49,7 +54,12 @@ public class GameManager : MonoBehaviour {
     private int first_attack_player;
     private int combat_player_1, combat_player_2;
 
-   
+    private enum splashType
+    {
+        turn,
+        battle,
+        end
+    };
     private int map_piece_no = 0;
 
     //used for various random assignments
@@ -89,10 +99,10 @@ public class GameManager : MonoBehaviour {
         playerObjects = GameObject.FindGameObjectsWithTag("Player"); 
         currentPlayer = 0;
 
+        mainCamera.enabled = true;
+        splashCamera.enabled = false;
+
        // Debug.Log("Start log");
-
-
-        //Array.Copy(cameras, cameraTemp, cameras.Length);
         
     }
 
@@ -126,20 +136,13 @@ public class GameManager : MonoBehaviour {
         SendAirConsoleMessage(AirConsole.instance.ConvertPlayerNumberToDeviceId(2), "player_color", "color", "green");
         SendAirConsoleMessage(AirConsole.instance.ConvertPlayerNumberToDeviceId(3), "player_color", "color", "yellow");
         
-        /*for (int i = 0; i < cameraTemp.Length-1; i++)
-        {
-            cameraTemp[i].enabled = false;
-        }
-        cameraTemp[0] = cameras[currentPlayer];
-        cameraTemp[count].enabled = true;*/
-        StartCoroutine("wait");
-        //Debug.LogWarning(cameraTemp[currentPlayer]);
-        
         //Debug.Log("Player count: " + playerCount);
 
 
         //Debug.Log("Starting game for device no. " + AirConsole.instance.ConvertPlayerNumberToDeviceId(currentPlayer));
         SendAirConsoleMessage(AirConsole.instance.ConvertPlayerNumberToDeviceId(currentPlayer), "turn");
+        SetSplashScreen(currentPlayer, splashType.turn);
+        StartCoroutine("ChangeCamera");
         isWaiting = true;
 
     }
@@ -245,16 +248,10 @@ public class GameManager : MonoBehaviour {
         {
             count++; //increase turn counter
             currentPlayer = turnOrder[count]; //update current player
-            
-            //cameraTemp[count] = cameras[currentPlayer];
-            /*for (int i = 0; i < 4; i++)
-            {
-                        cameraTemp[i].enabled = false;
-            }*/
             StartCoroutine("wait");
             print(currentPlayer);
-            //Debug.LogWarning(cameras[currentPlayer]);
-            
+            StartCoroutine("ChangeCamera");
+            SetSplashScreen(currentPlayer, splashType.turn);
         }
         else //turn order is depleted
         {
@@ -263,9 +260,7 @@ public class GameManager : MonoBehaviour {
             currentPlayer = turnOrder[0]; //update current player
             //Debug.LogWarning(currentPlayer);
             count = 0; //reset turn counter
-            //cameraTemp[count] = cameras[currentPlayer];
             StartCoroutine("wait");
-            //Debug.LogWarning(cameraTemp[currentPlayer]);
         }
 
         SendLogMessageToFile(0, currentPlayer.ToString());
@@ -295,16 +290,6 @@ public class GameManager : MonoBehaviour {
             turnOrder[1] = temp;
         }
         
-    }
-
-    
-    IEnumerator wait()
-    {
-        //cameraTemp[count].enabled = true;
-        Debug.LogWarning("wait started");
-        yield return new WaitForSecondsRealtime(3);
-        //cameraTemp[count].enabled = false;
-        Debug.LogWarning("wait done");
     }
 
     /// <summary>
@@ -387,9 +372,16 @@ public class GameManager : MonoBehaviour {
 
             //Send win message and map to appropriate player
             if (device_id != combat_player_1)
+            {
                 SendAirConsoleMessage(combat_player_1, "combat_result_won", "map", data["map"]);
+                SetSplashScreen(AirConsole.instance.ConvertDeviceIdToPlayerNumber(combat_player_1), splashType.battle);
+            }
             else
+            {
                 SendAirConsoleMessage(combat_player_2, "combat_result_won", "map", data["map"]);
+                SetSplashScreen(AirConsole.instance.ConvertDeviceIdToPlayerNumber(combat_player_2), splashType.battle);
+            }
+
         }
         //Response to "combat_result_loss" message. Losing player sends their map
         else if ((string)data["action"] == "map_piece_loss")
@@ -654,6 +646,30 @@ public class GameManager : MonoBehaviour {
             sw.WriteLine(message);
         }
     }
-    
 
+    void SetSplashScreen(int player, splashType splash)
+    {
+        switch (splash)
+        {
+            case splashType.battle:
+                imageToChange.sprite = combat[player];
+                break;
+            case splashType.turn:
+                imageToChange.sprite = players[player];
+                break;
+            case splashType.end:
+                imageToChange.sprite = endGame[player];
+                break;
+        }
+   }
+
+    IEnumerator ChangeCamera()
+    {
+        mainCamera.enabled = !mainCamera.enabled;
+        splashCamera.enabled = !splashCamera.enabled;
+        yield return new WaitForSecondsRealtime(3);
+        mainCamera.enabled = !mainCamera.enabled;
+        splashCamera.enabled = !splashCamera.enabled;
+    }
+    
 }
